3fac894c2f9e2e9ab34ed9fd1565749ee03eaa1c
James Kozlowski <jameskozlowski@gmail.com>
2017-03-05 13:36:57 -0400

    Better commenting

--- DoomWad.java
+++ DoomWad.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 
 /******************************************************************************************
 * This class holds a DOOM Wad wadFile
-* Wad structure from https://zdoom.org/wiki/WAD
+* Wad structure from http://doom.wikia.com/wiki/WAD
 *
 *Author James Kozlowski
 ******************************************************************************************/
@@ -25,7 +25,6 @@ public class DoomWad
     //wad file ioStream
     RandomAccessFile ioStream = null;
 
-
     //entery point for the simple extractor
     public static void main(String[] args)
     {
@@ -45,12 +44,16 @@ public class DoomWad
 
         try
         {
+            //open up the IO Stream
             ioStream = new RandomAccessFile(fileName, "r");
 
+            //read the heder of the file
             ReadHeader();
 
+            //read the wad file directory
             ReadDirectory();
 
+            //extract the files from the WAD file
             extractAllFiles();
         }
         catch (FileNotFoundException ex)
@@ -66,6 +69,7 @@ public class DoomWad
         {
             try 
             {
+                //close the stream
                 ioStream.close();
             } 
             catch (Exception e) 
@@ -85,14 +89,18 @@ public class DoomWad
     **************************************************************************************************************/
     private void ReadHeader() throws IOException
     {
+        //all data in the header are 4 bytes long
         byte[] headerData = new byte[4];
 
+        //read in the WAD type
         ioStream.read(headerData);
         WADType = new String(headerData, "UTF-8");
 
+        //Read in the number of files in the wad directory
         ioStream.read(headerData);
         directoryLength = java.nio.ByteBuffer.wrap(headerData).order(java.nio.ByteOrder.LITTLE_ENDIAN).getInt();
         
+        //get the pointer to the directory 
         ioStream.read(headerData);
         directoryPointer = java.nio.ByteBuffer.wrap(headerData).order(java.nio.ByteOrder.LITTLE_ENDIAN).getInt();
     }
@@ -113,25 +121,32 @@ public class DoomWad
     private void ReadDirectory() throws IOException
     {
 
+        //create an array list for the files in the directory
         wadFiles = new ArrayList<WadDirectoryFile>();
 
+        //go to the start of the directory
         ioStream.seek(directoryPointer);
         
-        //Read the directory in 
+        //Read the directory information for each item
         for (int i = 0; i < directoryLength; i++)
         {
+
             byte[] dirData = new byte[4];
 
+            //read in the pointer to the file
             ioStream.read(dirData);
             int Pointer = java.nio.ByteBuffer.wrap(dirData).order(java.nio.ByteOrder.LITTLE_ENDIAN).getInt();
             
+            //read in the size of the file
             ioStream.read(dirData);
             int Size = java.nio.ByteBuffer.wrap(dirData).order(java.nio.ByteOrder.LITTLE_ENDIAN).getInt();
             
+            //read in the name of the file
             dirData = new byte[8];
             ioStream.read(dirData);
             String Name = new String(dirData, "UTF-8");
             
+            //add the file information to the ArrayList
             wadFiles.add(new WadDirectoryFile(Pointer, Size, Name));
         }     
     }
@@ -139,28 +154,41 @@ public class DoomWad
     //Extract all files from the doom WAD file
     private void extractAllFiles() throws IOException
     {
+        //create a directory for the extracted files
         File dir = new File(fileName +  " Files");
 
         if (!dir.exists())
             dir.mkdir();
         
+        //loop over each item in the directory list
         for (int i = 0; i < directoryLength; i++)
         {
             WadDirectoryFile file = wadFiles.get(i);
             
+            //only if it is a real file and not a marker(0 bytes)
             if (file.getLength() > 0)
             {
 
+                //create file
                 RandomAccessFile fileOutStream = new RandomAccessFile("./" + dir + "/" + file.getName(), "rw");
                 
+                //create an byte buffer for the file
+                //as WAD files are not very large we dont need to wory about the buffer being too large
                 byte[] data = new byte[file.getLength()];
+                
+                //go to the start of the file in the WAD
                 ioStream.seek(file.getStartPointer());
 
+                //read in the data to the buffer
                 ioStream.read(data);
+
+                //save the buffer to the new file
                 fileOutStream.write(data);
 
+                //close out the new file stream
                 fileOutStream.close();
 
+                //print out the name of the new file created
                 System.out.println(dir + "/" + file.getName());
             }
         }
